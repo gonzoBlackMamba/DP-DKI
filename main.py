cbe61aa7501681230fc8d616254ea570a5a07390
gonzoBlackMamba <huntermoss88@gmail.com>
2024-03-13 16:31:20 -0400

    All cleaned

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+
+""" Runs the DKI-ViSTa calculation
+"""
+from packages import *
+from functions import *
+from flags import *
+
+plt.rcParams["font.family"] = "Times New Roman"
+
+
+def main():
+  
+  if __name__ == "__main__":
+    main()
